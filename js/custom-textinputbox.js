b57a42e14c2a6e481ce69f1fc73499e7a21835aa
Anna A Dominiak <a.dominiak@primemodule.com>
2015-03-19 15:43:27 +0100

    solution without jQuery

--- js/custom-textinputbox.js
+++ js/custom-textinputbox.js
@@ -1,29 +1,30 @@
-$().ready(function(){
+var LimitedInput = (function(){
 
-	$('div.word-count-textbox').each(function(){
-		var max = $(this).children("input[max-word-count]").attr("max-word-count");
-		$(this).children("span").text(max);
-	});
-});
+	var elementsWatched = document.getElementsByClassName('word-count-textbox');
+	for (var i = elementsWatched.length - 1; i >= 0; i--) {
+		var maxWords = elementsWatched[i].children[0].getAttribute('max-word-count');
+		elementsWatched[i].children[1].innerHTML = maxWords;
+	};
 
-$('div.word-count-textbox > input').keyup(function(event){
+	return {
+		limitInput: function(event){
 	
-	//remember cursor
-	var cursorStartPosition = event.currentTarget.selectionStart,
-		cursorEndPosition = event.currentTarget.selectionEnd;
+			//remember cursor
+			var cursorStartPosition = event.currentTarget.selectionStart,
+				cursorEndPosition = event.currentTarget.selectionEnd;
 
-	var maxWords = event.currentTarget.getAttribute('max-word-count');
-	var value = event.currentTarget.value;
-		wordCount = value.split(/\S+/).length - 1;
-	if (wordCount >= maxWords) {
-		var re = new RegExp("^\\s*\\S+(?:\\s+\\S+){0,"+(maxWords-1)+"}\\s*");
-		event.currentTarget.value = value.match(re);
-		wordCount = maxWords;
-		//restore cursor
-		event.currentTarget.setSelectionRange(cursorStartPosition, cursorEndPosition);
+			var maxWords = event.currentTarget.getAttribute('max-word-count');
+			var value = event.currentTarget.value;
+				wordCount = value.split(/\S+/).length - 1;
+			if (wordCount >= maxWords) {
+				var re = new RegExp("^\\s*\\S+(?:\\s+\\S+){0,"+(maxWords-1)+"}\\s*");
+				event.currentTarget.value = value.match(re);
+				wordCount = maxWords;
+				//restore cursor
+				event.currentTarget.setSelectionRange(cursorStartPosition, cursorEndPosition);
+			}
+			// set word counter label
+			event.currentTarget.nextElementSibling.innerHTML = maxWords-wordCount;
+		}
 	}
-	// set word counter label
-	$(event.currentTarget).siblings("span").text(maxWords-wordCount);
-});
-
-
+})();
