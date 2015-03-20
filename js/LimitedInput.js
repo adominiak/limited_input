2ff943902a1df7c76044f1c90edecbbe28bdb73b
Anna A Dominiak <a.dominiak@primemodule.com>
2015-03-20 11:54:22 +0100

    using querySelect, 'for' attribute removed, onkeyup handler rewritten

--- js/LimitedInput.js
+++ js/LimitedInput.js
@@ -4,26 +4,23 @@
 	};
 
 	LimitedInput.prototype._prepare = function(){
-		var elementsWatched = document.getElementsByClassName('word-count-textbox');
+		var elementsWatched = document.querySelectorAll('.word-count-textbox');
 		for (var i = elementsWatched.length - 1; i >= 0; i--) {
-			var inputElement = elementsWatched[i].children[0];
-			inputElement.addEventListener("keyup", this._limitInput);
+			var inputElement = elementsWatched[i].querySelector('input');
 			var maxWords = inputElement.getAttribute('max-word-count');
-			elementsWatched[i].children[1].innerHTML = maxWords;
+			inputElement.addEventListener("keyup", limit(event,maxWords), true);
+			elementsWatched[i].querySelector('span').innerHTML = maxWords;
 		};
 	}
 
-	LimitedInput.prototype._limitInput = function(event) {
-
-			//remember cursor
+	var limit = function(event, maxWords){
+		var re = new RegExp("^\\s*\\S+(?:\\s+\\S+){0,"+(maxWords-1)+"}\\s*");
+		return function(event){
 			var cursorStartPosition = event.currentTarget.selectionStart,
 				cursorEndPosition = event.currentTarget.selectionEnd;
-
-			var maxWords = event.currentTarget.getAttribute('max-word-count');
 			var value = event.currentTarget.value,
 				wordCount = value.split(/\S+/).length - 1;
 			if (wordCount >= maxWords) {
-				var re = new RegExp("^\\s*\\S+(?:\\s+\\S+){0,"+(maxWords-1)+"}\\s*");
 				event.currentTarget.value = value.match(re);
 				wordCount = maxWords;
 				//restore cursor
@@ -31,8 +28,8 @@
 			}
 			// set word counter label
 			event.currentTarget.nextElementSibling.innerHTML = maxWords-wordCount;
-		
-	};
+		}
+	}
 
 	window.TEST = window.TEST || {};
 	window.TEST.LimitedInput = LimitedInput;
